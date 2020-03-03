6d8ff781f9b95ad474ffeb23627425e406bc3336
jindaxiang <jindaxiang@163.com>
2020-03-03 20:50:17 +0800

    Add test module

---
+++ tests/test_demo.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Albert King
+date: 2020/3/3 20:46
+contact: jindaxiang@163.com
+desc: Just for test file
+"""
+
+
+def test_demo():
+    """
+    Just for test
+    :return: Error
+    :rtype: assert result
+    """
+    assert 1 == 1
+
+
+if __name__ == '__main__':
+    test_demo()
