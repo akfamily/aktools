283dd7479c611b1091e9abb7da020a524d98bf6d
jindaxiang <jindaxiang@163.com>
2021-12-09 17:32:43 +0800

    docs(docs): docs
    docs

--- aktools/__init__.py
+++ aktools/__init__.py
@@ -4,33 +4,21 @@
 Changelog
 
 0.0.1:for test
-
 0.0.2:FastAPI test
-
 0.0.3:FastAPI deployment test
-
 0.0.4:add docs
-
 0.0.5:add automate package management
-
 0.0.6:add automate package management success
-
 0.0.7:add automate release test
-
 0.0.8:add automate release test again
-
 0.0.9:add automate success
-
 0.0.10:fix: rename workflows
-
 0.0.11:fix: README.md
-
 0.0.12:fix: README.md
-
 0.0.13:fix: test pull_request
-
 0.0.14:fix: test master push tag
+0.0.15:fix: update PYPI info
 """
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 __author__ = "Albert King"
