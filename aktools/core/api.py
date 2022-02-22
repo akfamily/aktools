44e1ea445ab7ea99b3d6823ffd44380c1fea3a4e
jindaxiang <jindaxiang@163.com>
2022-02-22 18:35:59 +0800

    feat(login): add login support
    add login support

--- aktools/core/api.py
+++ aktools/core/api.py
@@ -6,16 +6,157 @@ Desc: HTTP 模式主文件
 """
 import json
 import urllib.parse
+from typing import Optional
 
 import akshare as ak
-from fastapi import FastAPI, Request, status
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Request
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 app = FastAPI()
 
+fake_users_db = {
+    "akshare": {
+        "username": "akshare",
+        "full_name": "AKShare AKFamily",
+        "email": "akfamily.akshare@gmail.com",
+        "hashed_password": "fakehashedakfamily",
+        "disabled": False,
+    },
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get("/api_token/{item_id}")
+def root(request: Request, item_id: str, current_user: User = Depends(get_current_active_user)):
+    """
+    接收请求参数及接口名称并返回 JSON 数据
+    此处由于 AKShare 的请求中是同步模式，所以这边在定义 root 函数中没有使用 asyncio 来定义，这样可以开启多线程访问
+    :param request: 请求信息
+    :type request: Request
+    :param item_id: 必选参数; 默认接口名 stock_zh_a_hist
+    :type item_id: str
+    :param current_user: 必选参数; 默认接口名 stock_zh_a_hist
+    :type current_user: str
+    :return: 指定 接口名称 和 参数 的数据
+    :rtype: json
+    """
+    interface_list = dir(ak)
+    decode_params = urllib.parse.unquote(str(request.query_params))
+    # print(decode_params)
+    if item_id not in interface_list:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "error": "未找到该接口，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+            },
+        )
+    eval_str = decode_params.replace("&", '", ').replace("=", '="') + '"'
+    if not bool(request.query_params):
+        try:
+            received_df = eval("ak." + item_id + f"()")
+            if received_df is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
+        except KeyError as e:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
+    else:
+        try:
+            received_df = eval("ak." + item_id + f"({eval_str})")
+            if received_df is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
+        except KeyError as e:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))@app.get("/api/{item_id}")
+
 
 @app.get("/api/{item_id}")
-def root(request: Request, item_id: str = "stock_zh_a_hist"):
+def root(request: Request, item_id: str):
     """
     接收请求参数及接口名称并返回 JSON 数据
     此处由于 AKShare 的请求中是同步模式，所以这边在定义 root 函数中没有使用 asyncio 来定义，这样可以开启多线程访问
@@ -28,6 +169,7 @@ def root(request: Request, item_id: str = "stock_zh_a_hist"):
     """
     interface_list = dir(ak)
     decode_params = urllib.parse.unquote(str(request.query_params))
+    # print(decode_params)
     if item_id not in interface_list:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -70,3 +212,8 @@ def root(request: Request, item_id: str = "stock_zh_a_hist"):
                 },
             )
         return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
+
+
+@app.get("/api/test-data/")
+async def test_data():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'result': 'get it', 'good': 'not'})
