d829d268a64405b3357b3f6922b3d2c21147602c
jindaxiang <jindaxiang@163.com>
2022-02-22 17:00:05 +0800

    feat(status code): add support response status code
    add support response status code

--- aktools/core/api.py
+++ aktools/core/api.py
@@ -1,25 +1,27 @@
 # -*- coding:utf-8 -*-
 # /usr/bin/env python
 """
-Date: 2021/12/9 19:05
+Date: 2022/2/22 16:05
 Desc: HTTP 模式主文件
 """
 import json
 import urllib.parse
 
 import akshare as ak
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/api/{item_id}")
-def root(request: Request, item_id: str):
+def root(request: Request, item_id: str = "stock_zh_a_hist"):
     """
-    接收接口名称及其参数并返回 JSON 数据
+    接收请求参数及接口名称并返回 JSON 数据
+    此处由于 AKShare 的请求中是同步模式，所以这边在定义 root 函数中没有使用 asyncio 来定义，这样可以开启多线程访问
     :param request: 请求信息
     :type request: Request
-    :param item_id: 必选参数; 接口名; 比如 stock_zh_a_hist
+    :param item_id: 必选参数; 默认接口名 stock_zh_a_hist
     :type item_id: str
     :return: 指定 接口名称 和 参数 的数据
     :rtype: json
@@ -27,23 +29,44 @@ def root(request: Request, item_id: str):
     interface_list = dir(ak)
     decode_params = urllib.parse.unquote(str(request.query_params))
     if item_id not in interface_list:
-        return {'error': '未找到该接口，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz'}
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "error": "未找到该接口，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+            },
+        )
     eval_str = decode_params.replace("&", '", ').replace("=", '="') + '"'
     if not bool(request.query_params):
         try:
             received_df = eval("ak." + item_id + f"()")
             if received_df is None:
-                return {'error': '该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz'}
-            temp_df = received_df.to_json(orient='records', date_format='iso')
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
         except KeyError as e:
-            return {'error': f'请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz'}
-        return json.loads(temp_df)
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
     else:
         try:
             received_df = eval("ak." + item_id + f"({eval_str})")
             if received_df is None:
-                return {'error': '该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz'}
-            temp_df = received_df.to_json(orient='records', date_format='iso')
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
         except KeyError as e:
-            return {'error': f'请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz'}
-        return json.loads(temp_df)
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
