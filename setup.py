eb2e7a19595d7e0f239b44f5ee347fdd978a3c5a
jindaxiang <jindaxiang@163.com>
2022-04-15 15:39:44 +0800

    feat(cors): add CORS support
    add CORS support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # /usr/bin/env python
 """
-Date: 2022/2/23 21:04
+Date: 2022/3/31 17:04
 Desc: AKTools 的 PYPI 基本信息文件
 """
 import re
@@ -39,7 +39,7 @@ setuptools.setup(
     url="https://github.com/akfamily/aktools",
     packages=setuptools.find_packages(),
     install_requires=[
-        "akshare>=1.2.55",
+        "akshare>=1.4.95",
         "fastapi>=0.70.0",
         "uvicorn>=0.16.0",
         "python-multipart>=0.0.5",
