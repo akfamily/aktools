9ccf34ad19a1437a283e37b46f511a496dc703b6
Albert <18234520+albertandking@users.noreply.github.com>
2025-10-29 21:20:06 +0800

    docs: update docs

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/env python
 """
-Date: 2024/3/26 15:05
+Date: 2025/10/29 20:05
 Desc: CLI 测试文件
 运行 `python -m pytest tests/test_cli.py` 进行测试
 """
@@ -16,6 +16,6 @@ def test_version() -> None:
     """
     测试版本
     """
-    result = runner.invoke(cli.app, ["--version"])
+    result = runner.invoke(cli.app, args=["--version"])
     assert result.exit_code == 0
     assert f"{__title__} v{__version__}\n" in result.stdout
